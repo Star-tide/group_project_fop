3c456ed0cf0313edf68dde00fb83e5b5041c047d
ERIC_BASKOVICH <eric.baskovich@gmail.com>
2024-08-28 08:01:13 -0600

    Created a course app and a class manager app to hold models, deleted the placeholder which had all but the user app. This is a proposed app structure for the project. User will need to be imported to the respective apps. Fields require validation and default values. Serializers will be needed once we are happy with the models.

--- backend/Course_app/apps.py
+++ backend/Course_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PlaceholderAppConfig(AppConfig):
+class CourseAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'placeholder_app'
+    name = 'Course_app'
--- backend/Course_app/models.py
+++ backend/Course_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-
+#USER_app (User)
 class User(models.Model):
     pass
 # Create your models here.
 
+#COURSE_app (Course, Question, Answer)
 class Course(models.Model):
     question_id = models.CharField()
     difficulty = models.CharField()
@@ -20,27 +21,8 @@ class Question(models.Model):
     learning_content = models.TextField()
     prompt = models.TextField()
     solution = models.TextField()
+    is_correct = models.BooleanField(default = False)
 
 class Answer(models.Model):
     user_solution = models.CharField()
     question = models.OneToOneField(Question)
-
-class Enrollment(models.Model):
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    student_id = models.ForeignKey(User, on_delete= models.CASCADE)
-
-
-class Certificate_completion(models.Model):
-    name = models.OneToOneField(User)
-    title = models.CharField()
-
-class Lesson_plan(models.Model):
-    title = models.CharField()
-    subcategories = ArrayField(models.CharField())
-    programming_language = models.CharField()
-    certificate_completion = models.OneToOneField(Certificate_completion)
-
-class Curriculum(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    lesson_plan = models.ForeignKey(Lesson_plan, on_delete=models.CASCADE)
