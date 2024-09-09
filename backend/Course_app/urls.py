08ea658c20235fc09f52cd366057fe1c320cedcb
rck5005 <rck5005@gmail.com>
2024-09-09 12:22:53 -0400

    added ability to create question into a course

--- backend/Course_app/urls.py
+++ backend/Course_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import CreateCourse
+from .views import CreateCourse, CreateQuestion
 
 
 urlpatterns = [
     # CreateCourse.asview
     path('create/', CreateCourse.as_view()),
+    path('question/', CreateQuestion.as_view()),
 ]
